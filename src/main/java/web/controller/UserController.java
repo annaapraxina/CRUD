5e0a4b1119912a3e584a2a99378302510c373d5c
annaapraxina <91375621+annaapraxina@users.noreply.github.com>
2021-11-03 00:21:17 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,58 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import web.model.User;
+import web.service.UserService;
+
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/users")
+    public String listUsers(Model model) {
+        model.addAttribute("allUsers", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping(value = "/users/new")
+    public String newUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", new User());
+        return "new";
+    }
+
+    @PostMapping(value = "/add")
+    public String addUser(@ModelAttribute User user) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String editUserForm(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PostMapping(value = "/edit")
+    public String editUser(@ModelAttribute User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/remove/{id}")
+    public String removeUser(@PathVariable("id") long id) {
+        userService.removeUserById(id);
+        return "redirect:/users";
+    }
+}
